4ed6726f84ec1c4ad7bb7811cdcb1d952f29a472
Eric-C-E <ericliu9969@outlook.com>
2025-12-20 13:27:15 -0500

    tried intellisense fix

--- main/app_display.c
+++ main/app_display.c
@@ -8,8 +8,6 @@
 
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
-#include "esp_lcd_panel_io_interface.h"
-
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
